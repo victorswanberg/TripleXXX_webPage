2af97791413bad19b02931443a01a9ae46850cbe
Victor Swanberg <victor.swanberg@sublimemedia.com>
2021-03-11 09:23:37 -0800

    Fixed npm issues

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+//scripts page
+
+//AOS data-scroll
+AOS.init();
+//Toggle button on carasouel component
+
+
+$(function () {
+  $(".carousel").carousel({ interval: 2000 });
+  $("#carouselButton").click(function () {
+    if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+      $(".carousel").carousel("pause");
+      $("#carouselButton").children("i").removeClass("fa-pause");
+      $("#carouselButton").children("i").addClass("fa-play");
+    } else {
+      $(".carousel").carousel("cycle");
+      $("#carouselButton").children("i").removeClass("fa-play");
+      $("#carouselButton").children("i").addClass("fa-pause");
+    }
+  });
+});
