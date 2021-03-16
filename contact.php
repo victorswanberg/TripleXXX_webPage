9bf29c7a968de603f39e9a36ac1bf7583d19dcf0
Chase Lones <chaselones@gmail.com>
2021-03-16 12:34:19 -0700

    changed contactus.html to contactus.php
    Changed file extension to be PHP, added PHP script with email and message validation. Using my personal email for testing purposes for now.

--- contact.php
+++ contact.php
@@ -1,5 +1,31 @@
-<!doctype html>
+<!-- <!doctype html> -->
 <html lang="en-us">
+
+<?php
+
+	$message_sent = false;
+
+	if(isset($_POST['user_email'])&& $_POST['user_email'] != ''){
+
+		if(filter_var($_POST['user_email'],FILTER_VALIDATE_EMAIL)&& $_POST['user_message']!=''){
+			$userName = $_POST['user_name'];
+			$userEmail = $_POST['user_email'];
+			$userMessage = $_POST['user_message'];
+
+			$to = "chaselones@gmail.com";
+			$subject = "message from site";
+			$body = "";
+
+			$body .= "From: ".$userName. "\r\n";
+			$body .= "Email: ".$userEmail. "\r\n";
+			$body .= "Message: ".$message. "\r\n";
+
+			mail($to,$subject,$body);
+
+
+		}
+	}
+?>
 
 <head>
 	<!-- Required meta tags -->
@@ -56,29 +82,43 @@
 	<h1 class="col-12 text-center text-dark" id="contactParagraph">
 		Please fill out the form below to leave us any feedback!
 	</h1>
-	<form class="row text-center" id="contactForm">
+
+	<?php
+		if($message_sent):
+	?>
+
+	<h1>THANK YOU FOR YOUR FEEDBACK!  YOUR MESSAGE HAS BEEN SENT!</h1>
+
+	<?php
+	else:
+	?>
+
+	<form class="row text-center" id="contactForm" action="contact.php" method="POST">
 		<div class="col-12 form-group">
 			<label for="user_name">
 				<h3>Your name:</h3>
 			</label>
-			<input id="user_name" type="text" class="mx-auto form-control col-6" placeholder="Your Name">
+			<input id="user_name" type="text" class="mx-auto form-control col-6" placeholder="Your Name" name="user_name">
 		</div>
 		<div class="form-group col-12">
 			<label for="user_email">
 				<h3>Your Email:</h3>
 			</label>
-			<input type="email" class="mx-auto form-control col-6" id="user_email" placeholder="name@example.com">
+			<input type="email" class="mx-auto form-control col-6" id="user_email" placeholder="name@example.com" name="user_email">
 		</div>
 		<div class="form-group col-12">
 			<label for="user_message">
 				<h3>Comments/Questions:</h3>
 			</label>
-			<textarea class="mx-auto form-control col-6" id="user_message" rows="3"></textarea>
+			<textarea class="mx-auto form-control col-6" id="user_message" rows="3" name="user_message"></textarea>
 		</div>
 		<div class="form-group col-12">
 			<button class="btn btn-danger btn-lg" type="submit">Submit</button>
 		</div>
 	</form>
+	<?php
+	endif;
+	?>
 
 	<!--footer start-->
 	<footer class="container-fluid">
